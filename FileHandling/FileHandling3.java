8c1c267811c3d8d213ebfd415007f99f6e33d0f5
Ashfaq <105435085+DarkSharkAsh@users.noreply.github.com>
2023-03-24 10:14:58 +0530

    File handling

---
+++ FileHandling/FileHandling3.java
@@ -0,0 +1,136 @@
+package FileHandling;
+
+import java.io.IOException;
+import java.io.BufferedInputStream;
+
+import java.io.BufferedOutputStream;
+
+import java.io.FileInputStream;
+
+import java.io.FileNotFoundException;
+
+import java.io.FileOutputStream;
+
+import java.io.IOException;
+
+import java.util.Scanner;
+public class FileHandling3 {
+
+	// Recommended as this is efficient using Fileinputstream , Fileoutputstream
+
+    // with bufferedinputstream and buffredoutputstream
+
+
+
+    public static void main(String[] args) throws IOException {
+
+
+
+           // FileOutputStream outputStream = new FileOutputStream("Advanced.txt");
+
+           //
+
+           // BufferedOutputStream outputStream2 = new BufferedOutputStream(outputStream);
+
+           //
+
+           // String demo="Java is a high-level, class-based, object-oriented programming
+
+           // language that is designed to have as few implementation dependencies as
+
+           // possible. It is a general-purpose programming language intended to let
+
+           // programmers write once, run anywhere (WORA),[17] meaning that compiled Java
+
+           // code can run on all platforms that support Java without the need to
+
+           // recompile.[18] Java applications are typically compiled to bytecode that can
+
+           // run on any Java virtual machine (JVM) regardless of the underlying computer
+
+           // architecture. The syntax of Java is similar to C and C++, but has fewer
+
+           // low-level facilities than either of them. The Java runtime provides dynamic
+
+           // capabilities (such as reflection and runtime code modification) that are
+
+           // typically not available in traditional compiled languages. As of 2019, Java
+
+           // was one of the most popular programming languages in use according to
+
+           // GitHub,[19][20] particularly for client?server web applications, with a
+
+           // reported 9 million developers.[21]\r\n"
+
+           // + "\r\n"
+
+           // + "Java was originally developed by James Gosling at Sun Microsystems. It was
+
+           // released in May 1995 as a core component of Sun Microsystems' Java platform.
+
+           // The original and reference implementation Java compilers, virtual machines,
+
+           // and class libraries were originally released by Sun under proprietary
+
+           // licenses. As of May 2007, in compliance with the specifications of the Java
+
+           // Community Process, Sun had relicensed most of its Java technologies under the
+
+           // GPL-2.0-only license. Oracle offers its own HotSpot Java Virtual Machine,
+
+           // however the official reference implementation is the OpenJDK JVM which is
+
+           // free open-source software and used by most developers and is the default JVM
+
+           // for almost all Linux distributions.\r\n"
+
+           // + "\r\n"
+
+           // + "As of September 2022, Java 19 is the latest version, while Java 17, 11 and
+
+           // 8 are the current long-term support (LTS) versions";
+
+           // outputStream2.write(demo.getBytes());
+
+           // outputStream2.close();//closing from last resources
+
+           // outputStream.close();
+
+
+
+           //            FileInputStream inputStream = new FileInputStream("Advanced.txt");
+
+           //
+
+           //            BufferedInputStream inputStream2 = new BufferedInputStream(inputStream);
+
+           //
+
+           //            int a = 0;
+
+           //            while ((a = inputStream2.read()) != -1) {
+
+           //                  System.out.print((char) a);
+
+           //            }
+
+
+
+           // use anyone meth
+
+           //            Scanner scanner = new Scanner(inputStream2);
+
+           //            while (scanner.hasNext()) {
+
+           //                  String nextLine = scanner.nextLine();
+
+           //                  System.out.println(nextLine);
+
+           //            }
+
+
+
+    }
+
+
+}
